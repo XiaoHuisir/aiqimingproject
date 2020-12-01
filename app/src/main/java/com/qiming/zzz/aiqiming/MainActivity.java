eaed434360b7472b0ce92b3af75ee3b41d43e75d
XiaoHuisir <860663594@qq.com>
2020-12-01 21:53:39 +0800

    Initial commit

---
+++ app/src/main/java/com/qiming/zzz/aiqiming/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qiming.zzz.aiqiming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
